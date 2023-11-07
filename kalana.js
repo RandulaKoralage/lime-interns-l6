56536bd5caf99f49d4334301accfd3cf24a80775
Hasala Dissanayake <hasala@linearsix.com>
2023-11-07 11:44:46 +0530

    added a try catch

--- kalana.js
+++ kalana.js
@@ -2,13 +2,21 @@ function findSecondLargestElem(arr){
     let first = -1 , second = -1;
 
     for(let i = 0; i <= arr.length-1; i++){
-        if(arr[i] > first){
-            second = first;
-            first = arr[i];
+        try{
+            if(arr[i] > first){
+                second = first;
+                first = arr[i];
+            }
+            else if( arr[i] > second && arr[i] != first){
+                second = arr[i];
+            }
+        } catch (e) {
+            console.log(e);
+        } finally {
+            console.log("finally this happened");
         }
-        else if( arr[i] > second && arr[i] != first){
-            second = arr[i];
-        }
+
+
     }
     console.log(second);
 }
